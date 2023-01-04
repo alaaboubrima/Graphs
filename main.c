4bbbcf4794f8023084d05fb31a1d5f11fb798c72
alaaboubrima <97070504+alaaboubrima@users.noreply.github.com>
2023-01-04 17:01:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #define MAX_SOMMETS 100
+#include <time.h>
 
 // Matrice d'adjacence pour représenter un graph
 int matrice_adj[MAX_SOMMETS][MAX_SOMMETS];
@@ -74,31 +75,76 @@ bool rech_chemin(int origine, int dest) {
 
   return false;
 }
-      
+  
+  
+// Ajouter un nœud avec ses liens au graph
+void add_node_with_links(int node, int num_links, int links[]) {
+  for (int i = 0; i < num_links; i++) {
+    matrice_adj[node][links[i]] = 1;
+    matrice_adj[links[i]][node] = 1;
+  }
+}    
+
+
+// Supprimer un nœud avec ses liens
+void delete_node_with_links(int node) {
+  // Supprimer les liens du nœud
+  for (int i = 0; i < num_sommets; i++) {
+    matrice_adj[node][i] = 0;
+    matrice_adj[i][node] = 0;
+  }
+  // Supprimer une colonne de la matrice
+  for (int i = 0; i < num_sommets; i++) {
+    for (int j = node; j < num_sommets - 1; j++) {
+      matrice_adj[i][j] = matrice_adj[i][j + 1];
+    }
+  }
+  // Supprimer une ligne de la matrice
+  for (int i = node; i < num_sommets - 1; i++) {
+    for (int j = 0; j < num_sommets; j++) {
+      matrice_adj[i][j] = matrice_adj[i + 1][j];
+    }
+  }
+}
+
+
+
+
+
+
+/////////////////////////////////
+
+void stop(clock_t start) {
+  // Arrêter le chronomètre
+  clock_t end = clock();
+
+  // Calculer le temps d'exécution en secondes
+  double elapsed_time = (end - start) / (double)CLOCKS_PER_SEC;
+
+  // Afficher le résultat
+  printf("Temps d'exécution : %f secondes\n", elapsed_time * 1000000);
+
+}
+///////////////////////////////
       
 int main(void) {
+  clock_t start = clock();
+  stop(start);
   // Initialiser le graph avec 4 sommets
   num_sommets = 4;
   construct_graph(num_sommets);
 
   // Ajouter quelques arêtes
-  add_arc(0, 1);
-  add_arc(0, 2);
-  add_arc(2, 1);
-  add_arc(1, 2);
-  add_arc(2, 3);
+  add_arc(1,2);
 
 
-  // Représentation du graph
-  rep_graph(num_sommets);
+
 
   // Densite
   printf("%f\n", densite(num_sommets));
   
   // Rechercher un nœud dans le graph
   rech_sommet(3);
-  
-  
   
   // Rechercher un chemin entre les nœuds 0 et 3
   if (rech_chemin(0, 3)) {
@@ -107,6 +153,21 @@ int main(void) {
     printf("Chemin non trouvé\n");
   }
   
+  num_sommets++;
+  int node2_links[] = { 3 };
+  add_node_with_links(2, 1, node2_links);
   
+  delete_node_with_links(0);
+  num_sommets--;
+
+
+
+  // Représentation du graph
+  rep_graph(num_sommets);
   return 0;
 }
+
+
+
+
+
